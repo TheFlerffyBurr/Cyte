9063897511a527fb5875dc863b6a78a417fa4e42
mechrizon <37358412+mechrizon@users.noreply.github.com>
2018-03-15 04:03:50 +0800

    Add files via upload
    Simple AI Behaviour

---
+++ playerControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour {
+
+	public float BaseSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if(Input.GetKey(KeyCode.A)){
+			pos.x -=BaseSpeed * Time.deltaTime;
+			transform.position = pos;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			pos.x +=BaseSpeed * Time.deltaTime;
+			transform.position = pos;
+		}
+		if(Input.GetKey(KeyCode.W)){
+			pos.z +=BaseSpeed * Time.deltaTime;
+			transform.position = pos;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			pos.z -=BaseSpeed * Time.deltaTime;
+			transform.position = pos;
+		}
+	}
+}
