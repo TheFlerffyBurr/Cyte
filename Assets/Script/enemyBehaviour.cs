93bb5a450d049c18ff9429147ac0dc8aac6eb10d
mechrizon <37358412+mechrizon@users.noreply.github.com>
2018-03-20 06:18:28 +0800

    Add files via upload

---
+++ Assets/Script/enemyBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehaviour : MonoBehaviour {
+
+	public GameObject target;
+	public float BaseSpeed = 1f;
+	public float minPeriod = 1f;
+	public float maxPeriod = 5f;
+	public float period = 3f;
+	public float distance = 100f;
+	public float chasingSpeed = 2f;
+	private float dist;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dist = Vector3.Distance (target.transform.position,transform.position);
+		if (dist < distance) {
+			FollowState ();
+		} else {
+			NormalState ();
+		}
+			
+	}
+
+	private void NormalState(){
+		//forward movement
+		Transform pos = transform;
+		pos.position += pos.forward * Time.deltaTime * BaseSpeed;
+		transform.position = pos.position;
+
+		//change in angle rotation every few second
+		period -= Time.deltaTime;
+		if (period <= 0f) {
+			period = Random.Range(minPeriod,maxPeriod);
+			Transform angle = transform;
+			angle.Rotate (0,Random.Range(-180,180),Random.Range(-180,180));
+			transform.rotation = angle.rotation;
+		};
+			
+	} 
+
+	private void FollowState(){
+		
+		Transform pos = transform;
+		pos.position += pos.forward * Time.deltaTime * BaseSpeed * chasingSpeed;
+		transform.position = pos.position;
+
+		transform.LookAt (target.transform);
+
+	}
+
+
+}
---
+++ Assets/Script/playerControls.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour {
+
+	public float BaseSpeed = 5f;
+	public float Sensitivity =10f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+		followMouse ();
+	}
+
+	private void followMouse(){
+		float mouseX = Input.GetAxis("Mouse X") * Sensitivity;
+		float mouseY = Input.GetAxis("Mouse Y") * Sensitivity;
+
+		Transform angle = transform;
+		Vector3 mouse = new Vector3(-mouseY,mouseX,0f); 
+		angle.Rotate(mouse);
+		angle.eulerAngles = new Vector3 (angle.eulerAngles.x, angle.eulerAngles.y, 0f);
+		transform.rotation = angle.rotation;
+	}
+
+	private void Movement(){
+		Transform pos = transform;
+		if(Input.GetKey(KeyCode.A)){
+			pos.position -=pos.right * BaseSpeed * Time.deltaTime;
+			transform.position = pos.position;
+		}
+		if(Input.GetKey(KeyCode.D)){
+			pos.position +=pos.right * BaseSpeed * Time.deltaTime;
+			transform.position = pos.position;
+		}
+		if(Input.GetKey(KeyCode.W)){
+			pos.position +=pos.forward * BaseSpeed * Time.deltaTime;
+			transform.position = pos.position;
+		}
+		if(Input.GetKey(KeyCode.S)){
+			pos.position -=pos.forward * BaseSpeed * Time.deltaTime;
+			transform.position = pos.position;
+		}
+	}
+}
